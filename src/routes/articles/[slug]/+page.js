b1367131760576264bca278d14700f520b3dd5c6
slimehands <rodriguesorlen@gmail.com>
2024-05-30 09:14:59 -0400

    move story-files to articles

--- src/routes/articles/[slug]/+page.js
+++ src/routes/articles/[slug]/+page.js
@@ -1,5 +1,5 @@
 export async function load({ params }) { 
-	const post = await import(`../story-files/${params.slug}.svx`);
+	const post = await import(`../content/${params.slug}.svx`);
 	const {level, title} =  post.metadata;
 	const Content =  post.default;
 	const slug =  params.slug;
